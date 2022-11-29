3d01643117cdd1d08abe5042e95b764356802d43
jk1955 <jim@wecancodeit.org>
2022-11-29 16:38:12 -0500

    Reviewed Final Project

---
+++ src/Controllers/App.js
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import HomeIndex from '../Views/HomeIndex';
+import Player from '../Models/Player';
+
+export default function App() {
+  const requestURI = 'https://localhost:7117/api/Players/';
+
+  var idx = null;
+  var id = null;
+  const [refresh, setRefresh] = useState(() => false);
+  var [data, setData] = useState(() => []);
+  var player = new Player();
+  var players = data;
+  var showEdit = false;
+  var showCreate = false;
+  var showDelete = false;
+  var showDetails = false;
+
+  // GET
+  useEffect(() => {
+    fetch(requestURI)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setRefresh(false);
+      });
+  }, [refresh]);
+
+  players = data;
+  // console.log(players);
+
+  // PUT
+  function PutData(dataObj, id) {
+    fetch(requestURI + id, {
+      method: 'PUT',
+      body: JSON.stringify(dataObj),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    }).then((response) => {
+      if (response.status !== 204) {
+        console.log('PUT Failed', response);
+        return;
+      } else {
+        // console.log('PUT Succeeded', response);
+      }
+    });
+  }
+
+  // POST
+  function PostData(dataObj) {
+    // console.log(dataObj);
+    fetch(requestURI, {
+      method: 'POST',
+      body: JSON.stringify(dataObj),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    }).then((response) => {
+      if (response.status !== 201) {
+        console.log('POST Failed', response);
+        return;
+      } else {
+        setRefresh(true);
+        // console.log('POST Succeeded', players);
+      }
+    });
+  }
+  // DELETE
+
+  function DeleteData(dataObj, id) {
+    fetch(requestURI + id, {
+      method: 'DELETE',
+      body: JSON.stringify(dataObj),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    }).then((response) => {
+      setRefresh(true);
+      // console.log('DELETE response is ', response);
+    });
+  }
+
+  // console.log('PostData is ', PostData);
+  var selection = [
+    idx,
+    id,
+    player,
+    players,
+    showEdit,
+    showCreate,
+    showDelete,
+    showDetails,
+    PutData,
+    PostData,
+    DeleteData,
+  ];
+
+  return <HomeIndex data={selection} />;
+}
